e7a1bbf4e30a8ac8f75dbfa7a744995587c8943e
Chris Charles <chris@creatively.co.uk>
2022-10-14 12:11:58 +0100

    Project descriptions changes

--- src/App.tsx
+++ src/App.tsx
@@ -43,13 +43,13 @@ export default function App() {
         </section>
 
         <section className="projects">
-          <h2>Recent skills-updating projects</h2>
+          <h2>Recent 2022 skills-updating projects</h2>
           <ul className="projects-list">
             <li className="projects-list__chat">
               <div className="projects-list-item-container">
-                <h3>Local Climate Change Calculator</h3>
+                <h3>Location Climate Change Calculator</h3>
                 <img className="project__screenshot" alt="climate project screenshot" src={require("./images/screenshot-climate.png")} />
-                <div className="project__description">Calculates and graphs climate data for a given location, calculating the change in the last 10 years. It calls a node backend proxy API, that in turn calls a third-party past-weather data API</div>
+                <div className="project__description">Calculates and graphs changes in climate data over the last 10 years for any global location. It calls a Node backend proxy API, that in turn calls a third-party past-weather data API. It then processes over 3,650 days of temperature data on the fly for any user-specified location.</div>
                 <div className="project__skills-icons">
                   <img className="project__skills-icon" alt="javascript" src={require('./images/js-square-120h.png')} /> 
                   <img className="project__skills-icon" alt="typescript" src={require('./images/ts-square-120h.png')} /> 
@@ -79,7 +79,7 @@ export default function App() {
               <div className="projects-list-item-container">
                 <h3>Visual Content Analysis Tool</h3>
                 <img className="project__screenshot" alt="analysis project screenshot" src={require("./images/screenshot-analysis.png")} />
-                <div className="project__description">Transforms a text transcript into a visual representation of the emotional landscape of the text</div>
+                <div className="project__description">Transforms a text transcript into a visual representation of the emotional landscape of the text. Very abstracted file structure, with much React and Typescript useage. Uses 'Zustand' for global state management, along with 'Immer'</div>
                 <div className="project__skills-icons">
                   <img className="project__skills-icon" alt="javascript" src={require('./images/js-square-120h.png')} /> 
                   <img className="project__skills-icon" alt="typescript" src={require('./images/ts-square-120h.png')} /> 
